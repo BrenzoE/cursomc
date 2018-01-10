4eafcfd6c535aea459aa46eb39addc6e7e256954
BrenzoE <enzobelt78@gmail.com>
2018-01-10 00:38:22 -0200

    Criação da classe de produtos e sua associação com Categoria(ManyToMany)

--- src/main/java/com/brenzo/cursomc/CursomcApplication.java
+++ src/main/java/com/brenzo/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.brenzo.cursomc.domain.Categoria;
+import com.brenzo.cursomc.domain.Produto;
 import com.brenzo.cursomc.repositories.CategoriaRepository;
+import com.brenzo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,6 +29,19 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... arg0) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null,"Computador",2000);
+		Produto p2 = new Produto(null,"Impressora",800);
+		Produto p3 = new Produto(null,"Mouse",80);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 	}
 }

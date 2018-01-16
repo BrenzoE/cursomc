d053f23b847fe8917046ac8ef9fb53f4c807ae30
BrenzoE <enzobelt78@gmail.com>
2018-01-16 00:26:25 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/brenzo/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.brenzo.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brenzo.cursomc.domain.Pedido;
+import com.brenzo.cursomc.repositories.PedidoRepository;
+import com.brenzo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id + " e tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}

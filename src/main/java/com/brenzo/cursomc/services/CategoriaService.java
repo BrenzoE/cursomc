72f840aeb453d65a3057555a8e57c98aec52c5c8
BrenzoE <enzobelt78@gmail.com>
2018-01-20 15:15:50 -0200

    DELETE Categoria e exceptions

--- src/main/java/com/brenzo/cursomc/services/CategoriaService.java
+++ src/main/java/com/brenzo/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.brenzo.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.brenzo.cursomc.domain.Categoria;
 import com.brenzo.cursomc.repositories.CategoriaRepository;
+import com.brenzo.cursomc.services.exceptions.DataIntegrityException;
 import com.brenzo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel deletar uma Categoria que possui produtos");
+		}
+		
 	}
 }

198bb196d02e62006f3818ae4572d8a3d8d8e953
BrenzoE <enzobelt78@gmail.com>
2018-01-23 21:21:11 -0200

    Validação custumizada - CPF e CNPJ

--- src/main/java/com/brenzo/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/brenzo/cursomc/dto/ClienteNewDTO.java
@@ -2,21 +2,47 @@ package com.brenzo.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.brenzo.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max= 120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-	private String email;
-	private String cpfOuCnpj;
-	private Integer tipo;
 
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String cpfOuCnpj;
+	
+	private Integer tipo;
+	
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String complemento;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=3, max= 120, message="O tamanho deve ser entre 3 e 120 caracteres")
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
